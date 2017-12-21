7c317ff9fcfd7c57c0cc3b43746762e24f9d8919
Bret Comnes <bcomnes@gmail.com>
2017-12-21 11:40:54 -0800

    build me!

--- lambda/my-function.js
+++ lambda/my-function.js
@@ -1,11 +1,11 @@
 exports.handler = myHandlerFunction
 
-function myHandlerFunction (event, context, cb) {
-  var msg = "Hello world from AWS lambda at " + Date.now()
-  console.log(msg)
+function handler (event, context, cb) {
+  var message = "hello world from AWS " + Date.now()
+  // do some work
   cb(null, {
-    statusCode: 200
-    body: JSON.stringify({ msg: msg })
+    statusCode: 200,
+    body: JSON.stringify({ msg: message })
   })
 }
 
